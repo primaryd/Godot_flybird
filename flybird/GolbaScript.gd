ce0c11f48fd7ff3d9ec79de29cd63a21c75f13a3
dreamY <3350057050@QQ.com>
2024-11-06 19:56:16 +0800

    添加碰撞

--- flybird/GolbaScript.gd
+++ flybird/GolbaScript.gd
@@ -1,3 +1,4 @@
 extends Node
 
 signal del_pipe #水管清除信号
+signal game_over #游戏结束信号
--- flybird/pipe_move.gd
+++ flybird/pipe_move.gd
@@ -7,3 +7,9 @@ func _process(delta: float) -> void:
 	self.position.x-=move_speed*delta
 	if self.position.x<-100:
 		GolbaScript.del_pipe.emit(self)
+
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	GolbaScript.game_over.emit()
