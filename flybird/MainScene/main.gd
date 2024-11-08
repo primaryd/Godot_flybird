6e12513b5e2daf7271535e89d520acf71e1581a9
dreamY <3350057050@QQ.com>
2024-11-08 23:37:25 +0800

    添加restart按钮 用于重复进行游戏

--- flybird/MainScene/main.gd
+++ flybird/MainScene/main.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var score: Label = $CanvasLayer/Score
+@onready var bird: CharacterBody2D = $Bird
+
 
 
 
@@ -9,14 +11,15 @@ func _ready() -> void:
 	animation_player.play("defalt")
 	GolbaScript.game_over.connect(_game_over)
 	GolbaScript.game_start.connect(_game_start)
-	animation_player.play("Game_ready_in")
+	if GolbaScript.game_ready:
+		animation_player.play("Game_ready_in")
 	GolbaScript.record_score.connect(_record_score)
 
 
 
 func _game_over()->void:
 	GolbaScript.game_can_run=false
-	animation_player.play("Game_over")
+	animation_player.play("Game_over_in")
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_select") and not animation_player.is_playing():
@@ -31,3 +34,21 @@ func _game_start()->void:
 func _record_score()->void:
 	GolbaScript.score+=1
 	score.text="得分:"+str(GolbaScript.score)
+
+
+
+
+func _on_setting_button_down() -> void:
+	print("setting")
+	
+
+
+func _on_restart_button_down() -> void:
+	#重新载入游戏
+	get_tree().change_scene_to_file("res://MainScene/main.tscn")
+	GolbaScript.game_ready=false
+	GolbaScript.game_can_run=true
+	GolbaScript.score=0
+	#animation_player.play("Game_over_out")
+	
+	
